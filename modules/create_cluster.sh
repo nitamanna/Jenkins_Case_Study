e5544f4116bfda698e68b440211db203296bc4a5
Nita Manna <nitamanna@gmail.com>
2019-09-22 10:04:55 +0000

    updating grep command

--- modules/create_cluster.sh
+++ modules/create_cluster.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-aws ecs list-clusters | grep "airflow-fargate-cluster-jenkins" 2>&1 > /dev/null
+aws ecs list-clusters | grep airflow-fargate-cluster-jenkins 2>&1 > /dev/null
 status=$?
 if [[ ! "${status}" -eq 0 ]]; then
     aws ecs create-cluster --cluster-name airflow-fargate-cluster-jenkins
--- modules/create_log_grp.sh
+++ modules/create_log_grp.sh
@@ -3,19 +3,19 @@
 # Create Log Grour if not already exists
 
 
-aws logs describe-log-groups | grep 'web-log-group' 2>&1 > /dev/null
+aws logs describe-log-groups | grep web-log-group 2>&1 > /dev/null
 status=$?
 if [[ ! "${status}" -eq 0 ]]; then
     aws logs create-log-group --log-group-name /ecs/web-log-group
 fi
 
-aws logs describe-log-groups | grep 'mysql-log-group' 2>&1 > /dev/null
+aws logs describe-log-groups | grep mysql-log-group 2>&1 > /dev/null
 status=$?
 if [[ ! "${status}" -eq 0 ]]; then
     aws logs create-log-group --log-group-name /ecs/mysql-log-group
 fi
 
-aws logs describe-log-groups | grep 'airflow-log-group' 2>&1 > /dev/null
+aws logs describe-log-groups | grep airflow-log-group 2>&1 > /dev/null
 status=$?
 if [[ ! "${status}" -eq 0 ]]; then
     aws logs create-log-group --log-group-name /ecs/airflow-log-group
